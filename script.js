7c481d0a7a0370f1c4af9866710ca207f5792bc4
Landysh <landysh.stgr@bk.ru>
2023-07-18 09:56:03 +0300

    studied variables, alert and console.log

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = "lesson01";
+const screens = 5;
+let screenPrice = 200;
+const rollback = true;
+let fullPrice = 1200;
+const adaptive = null;
+
+alert('This is just a learning process');
+console.log('I warned you. This is just a task');
