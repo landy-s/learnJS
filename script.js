eb9b1748f9bbd811ef5cb0b70860ce26c7e49f74
Landysh <landysh.stgr@bk.ru>
2023-07-18 16:46:21 +0300

    the task is done

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const title = "lesson01";
-const screens = 5;
-let screenPrice = 200;
-const rollback = true;
-let fullPrice = 1200;
-const adaptive = null;
+const title = "lesson02";
+const screens = "Simple, Complex, Interactive";
+let screenPrice = 1200;
+const rollback = 95;
+let fullPrice = 100000000;
+const adaptive = true;
 
-alert('This is just a learning process');
-console.log('I warned you. This is just a task');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive); // тип данных значений переменных
+
+console.log(screens.length); // длину строки из переменной
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"); // Вывести в консоль
+
+console.log(screens.toLowerCase());
+console.log(screens.split(", ")); // Привести строку к нижнему регистру и разбить строку на массив
+
+let expression = fullPrice * (rollback/100);
+console.log(expression);
